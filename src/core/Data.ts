40678490cecd274d72d8552487e9091ab657344f
Duc Tai <leductai21203@gmail.com>
2026-01-30 16:57:03 +0700

    fix: countries

--- src/core/Data.ts
+++ src/core/Data.ts
@@ -7,29 +7,21 @@ export default [
     },
     endArray: [
       { name: "Japan", N: 35.6762, E: 139.6503 }, // Tokyo
-      { name: "Korea", N: 37.5665, E: 126.978 }, // Seoul
       { name: "Singapore", N: 1.3521, E: 103.8198 }, // Singapore
       { name: "Hong Kong", N: 22.3193, E: 114.1694 }, // Hong Kong
-      { name: "China", N: 39.9042, E: 116.4074 }, // Beijing
 
       // Europe
       { name: "United Kingdom", N: 51.5074, E: -0.1278 }, // London
       { name: "Germany", N: 52.52, E: 13.405 }, // Berlin
-      { name: "France", N: 48.8566, E: 2.3522 }, // Paris
       { name: "Netherlands", N: 52.3676, E: 4.9041 }, // Amsterdam
-      { name: "Sweden", N: 59.3293, E: 18.0686 }, // Stockholm
 
       // Americas
       { name: "United States", N: 38.9072, E: -77.0369 }, // Washington, D.C.
-      { name: "Canada", N: 45.4215, E: -75.6972 }, // Ottawa
-      { name: "Mexico", N: 19.4326, E: -99.1332 },
-      { name: "Brazil", N: -15.7975, E: -47.8919 }, // Brasília
 
       // Oceania
       { name: "Australia", N: -35.2809, E: 149.13 }, // Canberra
       { name: "New Zealand", N: -41.2865, E: 174.7762 }, // Wellington
 
-      { name: "United Arab Emirates", N: 24.4539, E: 54.3773 }, // Abu Dhabi
       { name: "Saudi Arabia", N: 24.7136, E: 46.6753 }, // Riyadh
     ],
   },

297022e743bb05971cb8794fcafed65c8d53de2c
Duc Tai <leductai21203@gmail.com>
2026-01-30 17:54:17 +0700

    fix: country

--- src/core/Data.ts
+++ src/core/Data.ts
@@ -6,22 +6,23 @@ export default [
       E: 105.8542,
     },
     endArray: [
-      { name: "Japan", N: 35.6762, E: 139.6503 }, // Tokyo
+      { name: "Japan", N: 35.6895, E: 139.6917 }, // Tokyo
+      { name: "Korea", N: 37.5665, E: 126.978 }, // Seoul
       { name: "Singapore", N: 1.3521, E: 103.8198 }, // Singapore
-      { name: "Hong Kong", N: 22.3193, E: 114.1694 }, // Hong Kong
 
       // Europe
       { name: "United Kingdom", N: 51.5074, E: -0.1278 }, // London
-      { name: "Germany", N: 52.52, E: 13.405 }, // Berlin
-      { name: "Netherlands", N: 52.3676, E: 4.9041 }, // Amsterdam
 
       // Americas
       { name: "United States", N: 38.9072, E: -77.0369 }, // Washington, D.C.
+      { name: "Mexico", N: 19.4326, E: -99.1332 }, // Mexico City
 
       // Oceania
       { name: "Australia", N: -35.2809, E: 149.13 }, // Canberra
       { name: "New Zealand", N: -41.2865, E: 174.7762 }, // Wellington
+      { name: "Germany", N: 52.52, E: 13.405 }, // Berlin
 
+      // Middle East
       { name: "Saudi Arabia", N: 24.7136, E: 46.6753 }, // Riyadh
     ],
   },

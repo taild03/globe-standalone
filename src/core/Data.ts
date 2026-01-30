d36253560342c8c003bac31f84e8891b3b43b99d
Duc Tai <leductai21203@gmail.com>
2026-01-30 16:53:07 +0700

    fix: countries

--- src/core/Data.ts
+++ src/core/Data.ts
@@ -1,36 +1,36 @@
 export default [
   {
     startArray: {
-      name: 'Hanoi',
+      name: "Hanoi",
       N: 21.0285,
       E: 105.8542,
     },
     endArray: [
-      { name: 'Seoul', N: 37.5665, E: 126.978 },
-      { name: 'Berlin', N: 52.52, E: 13.405 },
-      { name: 'Paris', N: 48.8566, E: 2.3522 },
-      { name: 'Mumbai', N: 19.076, E: 72.8777 },
-      { name: 'Tokyo', N: 35.6762, E: 139.6503 },
-      { name: 'Sydney', N: -33.8688, E: 151.2093 },
-      { name: 'Dubai', N: 25.2048, E: 55.2708 },
-      { name: 'London', N: 51.5074, E: -0.1278 },
-      { name: 'New York', N: 40.7128, E: -74.006 },
-      { name: 'Singapore', N: 1.3521, E: 103.8198 },
-      { name: 'Taiwan', N: 25.0330, E: 121.5654 },
-      { name: 'Hong Kong', N: 22.3193, E: 114.1694 },
-      { name: 'New Zealand', N: -41.2865, E: 174.7762 },
-      { name: 'Thailand', N: 13.7563, E: 100.5018 },
-      { name: 'Indonesia', N: -6.2088, E: 106.8456 },
-      { name: 'Philippines', N: 14.5995, E: 120.9842 },
-      { name: 'Cambodia', N: 11.5564, E: 104.9282 },
-      { name: 'Brazil', N: -15.7975, E: -47.8919 },
-      { name: 'Canada', N: 45.4215, E: -75.6972 },
-      { name: 'Mexico', N: 19.4326, E: -99.1332 },
-      { name: 'South Africa', N: -25.7479, E: 28.2293 }, // Pretoria
-      { name: 'Nigeria', N: 9.0765, E: 7.3986 },         // Abuja
-      { name: 'Chile', N: -33.4489, E: -70.6693 },       // Santiago
-      { name: 'Russia', N: 55.7558, E: 37.6173 },        // Moscow
-      { name: 'Ukraine', N: 50.4501, E: 30.5234 },       // Kyiv
+      { name: "Japan", N: 35.6762, E: 139.6503 }, // Tokyo
+      { name: "Korea", N: 37.5665, E: 126.978 }, // Seoul
+      { name: "Singapore", N: 1.3521, E: 103.8198 }, // Singapore
+      { name: "Hong Kong", N: 22.3193, E: 114.1694 }, // Hong Kong
+      { name: "China", N: 39.9042, E: 116.4074 }, // Beijing
+
+      // Europe
+      { name: "United Kingdom", N: 51.5074, E: -0.1278 }, // London
+      { name: "Germany", N: 52.52, E: 13.405 }, // Berlin
+      { name: "France", N: 48.8566, E: 2.3522 }, // Paris
+      { name: "Netherlands", N: 52.3676, E: 4.9041 }, // Amsterdam
+      { name: "Sweden", N: 59.3293, E: 18.0686 }, // Stockholm
+
+      // Americas
+      { name: "United States", N: 38.9072, E: -77.0369 }, // Washington, D.C.
+      { name: "Canada", N: 45.4215, E: -75.6972 }, // Ottawa
+      { name: "Mexico", N: 19.4326, E: -99.1332 },
+      { name: "Brazil", N: -15.7975, E: -47.8919 }, // Brasília
+
+      // Oceania
+      { name: "Australia", N: -35.2809, E: 149.13 }, // Canberra
+      { name: "New Zealand", N: -41.2865, E: 174.7762 }, // Wellington
+
+      { name: "United Arab Emirates", N: 24.4539, E: 54.3773 }, // Abu Dhabi
+      { name: "Saudi Arabia", N: 24.7136, E: 46.6753 }, // Riyadh
     ],
   },
 ];
